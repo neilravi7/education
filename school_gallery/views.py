8791611d4cfa1161f413510efdc3536c32613d77
Ravi Bagdi <ravibagdi799@outlook.com>
2025-03-17 13:24:31 +0530

    solved style related issue

--- school_gallery/views.py
+++ school_gallery/views.py
@@ -4,8 +4,20 @@ from .models import Gallery
 
 # Create your views here.
 
+home_response ="""
+<h1> work in progress <h2>
+<ul>
+<li>
+    <a href="/updates">NEWS</a>
+        </li>
+<li>
+    <a href="/gallery">GALLERY</a>
+</li>
+<ul/>
+"""
+
 def home(request):
-    return HttpResponse("<h1> work in progress <h2>")
+    return HttpResponse(home_response)
 
 def gallery(request):
     gallery = Gallery.objects.all()
